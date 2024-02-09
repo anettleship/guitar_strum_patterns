8ed573d228f7b4d1d918ca0c9693f97d4aeb2adf
Adrian Nettleship <adrian_nettleship@hotmail.com>
2024-02-09 14:26:16 +0000

    test_strum_pattern_generator_can_be_initilised

--- strumpatterns/tests/test_strum_pattern_generator.py
+++ strumpatterns/tests/test_strum_pattern_generator.py
@@ -1,2 +1,8 @@
-from strum_pattern_generator import StrumPatternGenerator
+from strumpatterns.strum_pattern_generator import StrumPatternGenerator
 
+
+def test_strum_pattern_generator_can_be_initilised():
+
+    strum_pattern_generator = StrumPatternGenerator()
+
+    assert strum_pattern_generator
