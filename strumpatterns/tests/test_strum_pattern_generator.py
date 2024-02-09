d95f4f93e779b1ab0f1304fc4cc326b459f3f5be
Adrian Nettleship <adrian_nettleship@hotmail.com>
2024-02-09 15:03:53 +0000

    commit failing test for next time

--- strumpatterns/tests/test_strum_pattern_generator.py
+++ strumpatterns/tests/test_strum_pattern_generator.py
@@ -1,10 +1,11 @@
 from strumpatterns.strum_pattern_generator import StrumPatternGenerator
 
 
-def test_strum_pattern_generator_generate_method_returns_string_passed_as_parameter():
+def test_strum_pattern_generator_generate_should_return_base64_encoded_png_data():
     input_string = "input_string"
     strum_pattern_generator = StrumPatternGenerator(input_string)
 
     result = strum_pattern_generator.generate()
-
-    assert result == input_string
+    
+    # iVBORw0KGg - https://stackoverflow.com/questions/62329321/how-can-i-check-a-base64-string-is-a-filewhat-type-or-not/62330081#62330081
+    assert result.startswith("iVBORw0KGg")
